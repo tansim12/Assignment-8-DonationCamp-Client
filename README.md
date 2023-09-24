d1234bb0111725e230e9e48e7cee062033388d57
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-09-25 03:39:14 +0600

    Readme file question added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+## Add at least 3 Project features
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div>
-            <div>
-            <h1>Oppps</h1>
-            </div>
-            <div>
-                <button className="btn btn-error">Home</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className=" flex flex-col justify-center items-center mt-36">
+      <div>
+        <h1 className="text-5xl my-5">Oppppsss</h1>
+      </div>
+      <div>
+        <Link to={"/"}>
+          <button className="btn btn-error text-white font-bold">Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
